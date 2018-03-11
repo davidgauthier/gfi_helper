ccae40bfcbb593dc58ec8d7edda9a6575e52f9c2
David GAUTHIER <davidgauthier75@gmail.com>
2018-03-11 03:36:55 +0100

    adding front Bootstrap4 elements to build on later

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -14,8 +14,8 @@ class AppController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('front/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
 }
